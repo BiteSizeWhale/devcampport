728dab5e0b557ce2ed6a127856a9f548d38cfee3
Jordan Hudgens <jordan@devcamp.com>
2017-06-27 15:25:51 +0000

    Added nav br and added default header content

---
+++ app/controllers/concerns/deflaut_page_conent.rb
@@ -0,0 +1,12 @@
+module DeflautPageConent
+  extend ActiveSupport::Concern
+  
+  included do 
+    before_filter :set_page_defaults
+  end
+  
+  def set_page_defaults
+    @page_title = "DevcampPortfolio | My Portfolio Website"
+    @seo_keywords = "Willliam Nichols Portfolio"
+  end
+end
